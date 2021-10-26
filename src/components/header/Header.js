124642ea6feadfaf98f2370870ee6869ade7de48
Anderson Jacondino <anderson.jacondino15@gmail.com>
2021-10-26 01:34:42 -0300

    add background
    Signed-off-by: Anderson Jacondino <anderson.jacondino15@gmail.com>

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,13 @@
 import ArrowDownIcon from "../../assets/images/icons/arrow-down.svg";
 import Logo from "../../assets/images/icons/logo-white.svg";
 import { PageScroll } from "../../util/pageScroll";
-import bgLeft from "../../assets/images/hero/desktop/left-leaves.png";
-import bgCenter from "../../assets/images/hero/desktop/plantperson.png";
-import bgRight from "../../assets/images/hero/desktop/right-leaves.png";
 
 export default () => {
   const Header = document.createElement("header");
 
   Header.setAttribute("class", "header");
-  // <div class="header__bg">
-  //   <div class="header__bg__img" id="header__bg1"><img src=${bgLeft} /></div>
-  //   <div class="header__bg__img" id="header__bg2"><img src=${bgCenter} /></div>
-  //   <div class="header__bg__img" id="header__bg3"><img src=${bgRight} /></div>
-  // </div>
 
   Header.innerHTML = `
-
   <div class="container">
     <div class="header__title">
       <img src=${Logo} alt="logo Green thumb" />
